a1dd090d4225a2bbfbc7ef2696ae0a66c826aa6f
Remi LEBRET <remi.lebret@idiap.ch>
2015-08-10 15:11:23 +0200

    removing some useless files

--- src/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-/*---- the configured options and settings for tax3Cpp ---------------*/
-
-// should we use VERBOSE option?
-/* #undef VERBOSE */
-
-#endif // CONFIG_H
